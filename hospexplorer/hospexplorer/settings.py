2b01f6ef36a540986f22e8218fc72e0c34c93646
Girik1105 <girik1105@gmail.com>
2026-01-29 11:35:22 -0700

    [HOP-4] Static root is now base_dir + staticfiles

--- hospexplorer/hospexplorer/settings.py
+++ hospexplorer/hospexplorer/settings.py
@@ -117,12 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STATIC_ROOT = [
-    BASE_DIR / "static"
-]
-print("STATIC")
-print(STATIC_ROOT)
 LLM_HOST = os.getenv("LLM_HOST", "mockserver")
 LLM_TOKEN = os.getenv("LLM_TOKEN", "")
 LLM_MODEL = os.getenv("LLM_MODEL", "")
