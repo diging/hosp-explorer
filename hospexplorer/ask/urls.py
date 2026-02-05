95db12f43489d8d18c110f8dfaaf6b9686416f8e
Girik Manchanda <74312030+Girik1105@users.noreply.github.com>
2026-02-05 12:12:54 -0700

    story/HOP-7 (#8)
    * [HOP-7] Added django-allauth, configured settings, added views and urls
    
    * [HOP-7] Templates
    
    * [HOP-7] Final templates, better comments in settings, uv lock
    
    * [HOP-7] comments
    
    * [HOP-7] Added login view to /

--- hospexplorer/ask/urls.py
+++ hospexplorer/ask/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
 from django.urls import path
 from ask import views
 
+app_name = "ask"
+
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="index"),
     path("mock", views.mock_response, name="mock-response"),
-    path("query", views.query, name="query-llm")
+    path("query", views.query, name="query-llm"),
 ]
