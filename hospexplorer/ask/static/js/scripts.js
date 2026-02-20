2a7ec64024170ebc63f971c847cda10badd96bd2
Girik1105 <girik1105@gmail.com>
2026-02-20 08:24:18 -0700

    [HOP-9] removed unecessary code

--- hospexplorer/ask/static/js/scripts.js
+++ hospexplorer/ask/static/js/scripts.js
@@ -3,26 +3,24 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-simple-sidebar/blob/master/LICENSE)
 */
-// 
+//
 // Scripts
-// 
+//
 
 window.addEventListener('DOMContentLoaded', event => {
 
     // Toggle the side navigation
-    const sidebarToggles = document.body.querySelectorAll('#sidebarToggle');
-
-    // Restore sidebar state from localStorage
-    if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-        document.body.classList.add('sb-sidenav-toggled');
-    }
-
-    sidebarToggles.forEach(toggle => {
-        toggle.addEventListener('click', event => {
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        // Uncomment Below to persist sidebar toggle between refreshes
+        // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
+        //     document.body.classList.toggle('sb-sidenav-toggled');
+        // }
+        sidebarToggle.addEventListener('click', event => {
             event.preventDefault();
             document.body.classList.toggle('sb-sidenav-toggled');
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
-    });
+    }
 
 });
